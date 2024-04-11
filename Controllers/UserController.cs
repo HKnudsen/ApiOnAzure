107bee50c2b9999bc6b3a9f7479d85c7b19df3fe
HKnudsen <henrik@csolutions.no>
2024-04-11 23:15:12 +0200

    Added Dapper data context

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Dapper;
+
+namespace ApiOnAzure.Controllers;
+
+
+[ApiController]
+[Route("controllers")]
+public class UserController: ControllerBase
+{
+
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,28 @@
+using System.Data;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace ApiOnAzure.Data
+{
+    class DataContext
+    {
+        private readonly IConfiguration _config;
+
+        public DataContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Query<T>(sql);
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.QuerySingle<T>(sql);
+        }
+    }
+}
